3348ebad8ac7ba396e46a70c98f9aa524424de57
soumyajyotibiswas <soumyajyoti@outlook.com>
2022-06-03 11:13:10 +0530

    100 days of code day1

---
+++ Code/Python/Learning/100_days_of_code+Udemy/0001_about_print_function.py
@@ -0,0 +1,10 @@
+"""
+Read more about the print function here - https://www.askpython.com/python/built-in-methods/python-print-function
+"""
+
+to_print="""
+Day1 - Python Print Function
+The function is declared like this:
+print('what to print')
+"""
+print(to_print)
